0531dbb02a9cd98b25058a2d6de5332a9dc0340f
Peramanathan <prem@weavler.it>
2016-02-26 17:35:38 +0100

    Updated technical timeline

--- features.js
+++ features.js
@@ -1,5 +1,5 @@
 {
-  "features":[
+  "features": [
     {
       "title": "Webshop (alpha)",
       "content": "This version should have following features",
@@ -9,32 +9,32 @@
     {
       "title": "Webshop (Beta)",
       "content": "This version should have following features",
-      "tasks": ["Signin and Signup", "Add and Update Profile", "Add and Update Product"],
+      "tasks": ["View Site", "Integrated Payments: Stripe and Paypal", "Default Theme"],
+      "date": "April 15, 2016"
+    },
+    {
+      "title": "Webshop (v1.0)",
+      "content": "This version should have following features",
+      "tasks": ["Unit test, integration test complaince", "Buyers reviews", "Custom Domain Purchase"],
       "date": "April 30, 2016"
     },
     {
-      "title": "Webshop (v1.0)",
+      "title": "Market Place (alpha)",
       "content": "This version should have following features",
-      "tasks": ["Signin and Signup", "Add and Update Profile", "Add and Update Product"],
-      "date": "March 30, 2016"
+      "tasks": ["Landing Page with call to action", "Basic Product Aggregation and categorization"],
+      "date": "May 15, 2016"
     },
     {
-      "title": "Webshop (v1.0)",
+      "title": "Copy shop (alpha)",
       "content": "This version should have following features",
-      "tasks": ["Signin and Signup", "Add and Update Profile", "Add and Update Product"],
-      "date": "March 30, 2016"
+      "tasks": ["Copy Shop Store", "Linking Favorite Products from marketplace"],
+      "date": "May 30, 2016"
     },
     {
-      "title": "Webshop (v1.0)",
+      "title": "Market Place (Beta)",
       "content": "This version should have following features",
-      "tasks": ["Signin and Signup", "Add and Update Profile", "Add and Update Product"],
-      "date": "March 30, 2016"
-    },
-    {
-      "title": "Webshop (v1.0)",
-      "content": "This version should have following features",
-      "tasks": ["Signin and Signup", "Add and Update Profile", "Add and Update Product"],
-      "date": "March 30, 2016"
+      "tasks": ["Robust Searchable", "Ads click Landing Pages", "Simple statistics"],
+      "date": "June 20, 2016"
     }
   ]
 }
